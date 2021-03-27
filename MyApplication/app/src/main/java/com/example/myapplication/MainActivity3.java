d12f554db1446f0c9744d55e1b8b5a1a3a684dd3
N-H-Arif <nokimulhasanarif256999@gmail.com>
2021-03-28 05:43:55 +0600

    profile added

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity3.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -4,9 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
@@ -22,15 +24,17 @@ public class MainActivity3 extends AppCompatActivity implements GoogleApiClient.
     GoogleApiClient googleApiClient;
 
     String SiteKey = "6LfaVpAaAAAAABseMYP7HrAJn1WX6xPTnTx1A728";
+    private Button BtnMove2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        checkBox=findViewById(R.id.check_Box);
 
-        googleApiClient=new GoogleApiClient.Builder(this)
+        checkBox = findViewById(R.id.check_Box);
+
+        googleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(SafetyNet.API)
                 .addConnectionCallbacks(MainActivity3.this)
                 .build();
@@ -40,26 +44,41 @@ public class MainActivity3 extends AppCompatActivity implements GoogleApiClient.
             @Override
             public void onClick(View view) {
 
-                if(checkBox.isChecked()){
-                    SafetyNet.SafetyNetApi.verifyWithRecaptcha(googleApiClient,SiteKey)
+                if (checkBox.isChecked()) {
+                    SafetyNet.SafetyNetApi.verifyWithRecaptcha(googleApiClient, SiteKey)
                             .setResultCallback(new ResultCallback<SafetyNetApi.RecaptchaTokenResult>() {
                                 @Override
                                 public void onResult(@NonNull SafetyNetApi.RecaptchaTokenResult recaptchaTokenResult) {
                                     Status status = recaptchaTokenResult.getStatus();
-                                    if((status != null) && status.isSuccess()){
+                                    if ((status != null) && status.isSuccess()) {
 
                                         Toast.makeText(getApplicationContext(), "Successfully Varified...", Toast.LENGTH_SHORT).show();
                                         checkBox.setTextColor(Color.GREEN);
                                     }
                                 }
                             });
-                }else{
+                } else {
                     checkBox.setTextColor(Color.BLACK);
                 }
-                            }
+            }
         });
 
+
+        BtnMove2 = findViewById(R.id.button3);
+        BtnMove2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                moveToActivityTwos();
+            }
+        });
     }
+
+    private void moveToActivityTwos() {
+        Intent intent = new Intent(com.example.myapplication.MainActivity3.this, MainActivity4.class);
+        startActivity(intent);
+    }
+
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
@@ -70,4 +89,9 @@ public class MainActivity3 extends AppCompatActivity implements GoogleApiClient.
     public void onConnectionSuspended(int i) {
 
     }
+
 }
+
+
+
+
---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity4.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+    }
+}
