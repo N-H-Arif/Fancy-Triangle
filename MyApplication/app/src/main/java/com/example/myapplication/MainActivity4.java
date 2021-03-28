24ce42957d6145a6104cf59da75a26885699782b
N-H-Arif <nokimulhasanarif256999@gmail.com>
2021-03-28 18:53:10 +0600

    chat

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity4.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity4.java
@@ -2,16 +2,35 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class MainActivity4 extends AppCompatActivity {
+
+    private Button BtnMove3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
+        BtnMove3 = findViewById(R.id.button9);
+        BtnMove3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                moveToActivityTwos();
+            }
+        });
     }
-}
+
+    private void moveToActivityTwos() {
+        Intent intent = new Intent(com.example.myapplication.MainActivity4.this, MainActivity5.class);
+        startActivity(intent);
+    }
+
+    }
