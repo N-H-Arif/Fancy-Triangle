87ae7722fa8e5ba598c4313a9498e077370a35da
N-H-Arif <nokimulhasanarif256999@gmail.com>
2021-03-29 02:39:51 +0600

    profile,goal

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity4.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity4.java
@@ -12,18 +12,39 @@ import android.widget.Spinner;
 public class MainActivity4 extends AppCompatActivity {
 
     private Button BtnMove3;
+    private Button BtnMove4;
+    private Button BtnMove5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
+        BtnMove4 = findViewById(R.id.button12);
         BtnMove3 = findViewById(R.id.button9);
+        BtnMove5 = findViewById(R.id.button4);
         BtnMove3.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 moveToActivityTwos();
+            }
+        });
+
+
+        BtnMove4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                moveToActivityThrees();
+            }
+        });
+
+        BtnMove5.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                moveToActivityFours();
             }
         });
     }
@@ -33,4 +54,16 @@ public class MainActivity4 extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void moveToActivityThrees() {
+        Intent intent = new Intent(com.example.myapplication.MainActivity4.this, MainActivity6.class);
+        startActivity(intent);
     }
+
+    private void moveToActivityFours() {
+        Intent intent = new Intent(com.example.myapplication.MainActivity4.this, MainActivity7.class);
+        startActivity(intent);
+    }
+
+
+
+}
